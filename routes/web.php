f5d1059b38cdd9a92f06f5171e082c910cb00246
Justin Reasoner <gjreasoner@gmail.com>
2025-05-19 06:43:06 -0400

    add links

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 
+Route::get("/", function () {
+    return view('welcome', [
+        'link' => Storage::disk('private')->url("object-storage-dir/test.txt"),
+        'signed' => Storage::disk('private')->temporaryUrl("object-storage-dir/test.txt", now()->addMinutes(5)),
+    ]);
+});
+
 Route::get('/list', function () {
-    return Storage::disk('private')->listContents("object-storage-dir");
+    return [
+        'files' => Storage::disk('private')->allFiles("object-storage-dir"),
+    ];
 });
 
 Route::get('/read', function () {
